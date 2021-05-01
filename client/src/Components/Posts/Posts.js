68d769aecbc4b0dd0d6e2ad659321cc944f4dc9a
8977Shiva <b.skreddy1997@gmail.com>
2021-05-01 12:28:20 +0530

    Client: structure of the app is done

---
+++ client/src/Components/Posts/Posts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Post from "./components/Post";
+
+const Posts = () => {
+  return (
+    <>
+      <h1>post </h1>
+      <Post />
+    </>
+  );
+};
+export default Posts;
